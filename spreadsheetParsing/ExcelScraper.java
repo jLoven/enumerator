d15a1a36e1b5301a2c568a731c08ede637777ecf
Jackie Loven <jloven@wpi.edu>
2015-07-29 21:44:47 -0500

    Writing to a new spreadsheet works

---
+++ spreadsheetParsing/ExcelScraper.java
@@ -0,0 +1,47 @@
+package spreadsheetParsing;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelScraper {
+
+
+	public static void main(String[] args) {
+		//  New workbook with blank sheet:
+		XSSFWorkbook workbook = new XSSFWorkbook(); 
+		XSSFSheet sheet = workbook.createSheet("Employee Data");
+
+		//  Some data to stick in the sheet:
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("hello");
+		data.add("world");
+
+		//  Put it in cells in the same row
+		int rowNumber = 0;
+		Row row = sheet.createRow(rowNumber);
+		int cellNumber = 0;
+		for (String word : data) {
+			Cell cell = row.createCell(cellNumber++);
+			cell.setCellValue(word);
+		}
+
+		//  Make the Excel workbook. Remember to close it!
+		try {
+			FileOutputStream out = new FileOutputStream(new File("first_generated_excel.xlsx"));
+			workbook.write(out);
+			out.close();
+			workbook.close();
+			System.out.println("first_generated_excel.xlsx written successfully!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
