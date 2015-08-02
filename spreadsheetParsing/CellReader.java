c6c105ba9c1c1e08af98a4ad2f6f640a0a553a6b
Jackie Loven <jloven@wpi.edu>
2015-08-02 01:40:50 -0500

    Able to read numeric values as well as string values from cells.

--- spreadsheetParsing/CellReader.java
+++ spreadsheetParsing/CellReader.java
@@ -1,3 +1,6 @@
+//  Jackie Loven, 2 August 2015
+
+
 package spreadsheetParsing;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,6 +16,11 @@ public class CellReader {
 		//  Cell type is number.
 		else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 			double number = cell.getNumericCellValue();
+			//  Assuming codes will often be integers and not doubles:
+			//  (Want to truncate added .0's on possible integers)
+			if (number == (int) number) {
+				return String.valueOf((int) number);
+			}
 			return String.valueOf(number);
 		}
 		//  Add others as needed.
--- spreadsheetParsing/SheetCreator.java
+++ spreadsheetParsing/SheetCreator.java
@@ -26,26 +26,24 @@ public class SheetCreator {
 		ArrayList<CodeValuePair> data = new ArrayList<CodeValuePair>();
 		CodeValuePair cvp1 = new CodeValuePair("Hello", "World");
 		CodeValuePair cvp2 = new CodeValuePair("123", "456");
+		data.add(cvp1);
+		data.add(cvp2);
 
-		//  Put it in cells in the same row
-		int rowNumber = 0;
-		int cellNumber = 0;
-		for (Object datum : data) {
-			Row row = sheet.createRow(rowNumber);
-			Cell cell = row.createCell(cellNumber++);
-
-			if (datum.getClass() == Double.class) {
-				cell.setCellType(Cell.CELL_TYPE_NUMERIC);
-				cell.setCellValue((Double) datum);
-			} else if (datum.getClass() == String.class) {
-				cell.setCellType(Cell.CELL_TYPE_STRING);
-				cell.setCellValue((String) datum);
-			}
-			if (cellNumber % 2 == 1) {
-				rowNumber++;
+		//  Codes go in column 0. Values go in column 1.
+		//  Currently this stores every item as a string. Does this matter?
+		for (int i = 0; i < data.size(); i++) {
+			Row row = sheet.createRow(i);
+			for (int j = 0; j < 2; j++) {
+			Cell cell = row.createCell(j);
+				if (j == 0) {
+					cell.setCellValue(data.get(i).getCode());
+				}
+				if (j == 1) {
+					cell.setCellValue(data.get(i).getValue());
+				}
 			}
 		}
-
+		
 		//  Make the Excel workbook. Remember to close it!
 		try {
 			FileOutputStream out = new FileOutputStream(new File("first_generated_excel.xlsx"));
@@ -56,7 +54,5 @@ public class SheetCreator {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
